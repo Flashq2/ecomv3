edfe0901e09ce7445534462b51371fc039c8078b
Flashq2 <pokputhea312@gmail.com>
2024-02-15 00:23:22 +0700

    Nothing to commit , Just Change Version

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -63,8 +63,8 @@
                         </div>
                     </div>
                     <div class="row  p-1">
-                        @for ($i = 0; $i < 10; $i++)
-                            <div class="col-6">
+                        @for ($i = 0; $i < 12; $i++)
+                            <div class="col-lg-3 col-md-4 col-6">
                                 <div class="product_box">
                                     <div class="block_image">
                                         <div class="block_control_img_style">
@@ -102,38 +102,42 @@
                             <span class="sub_detail_title_left active">SEE ALL <i
                                     class="fa-solid fa-chevron-right"></i></span>
                         </div>
-                        @for ($i = 0; $i < 10; $i++)
-                            <div class="list_body">
-                                <div class="list_body_box">
-                                    <div class="list_box_image">
-                                        <div class="image">
-                                            <img src="{{ asset('/image/mexican food mexican.jpg') }}" alt="">
+                        <div class="row">
+                            @for ($i = 0; $i < 12; $i++)
+                            <div class="col-lg-4 col-md-6 col-12 col-sm-12">
+                                <div class="list_body">
+                                    <div class="list_body_box">
+                                        <div class="list_box_image">
+                                            <div class="image">
+                                                <img src="{{ asset('/image/mexican food mexican.jpg') }}" alt="">
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="list_box_title">
-                                        <div class="top_title">
-                                            The Coca Cola
-                                        </div>
-                                        <div class="top_title_2">
-                                            Also known as covid -19 Antibody detection Test
-                                        </div>
-                                        <span class="rate_stare" style="color: #FAC213;"><i class="fa-solid fa-truck"></i>
-                                            Free
-                                            Delivery</span>
-                                        <div class="rate_stare">
-                                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
-                                                class="fa-solid fa-star"></i>
-                                            <i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half"></i>
-                                            ( <i class="fa-regular fa-user"></i> 6899)
-                                        </div>
-                                        <div class="add_button_of_list">
-                                            <button class="btn active">Add to Cart</button>
+                                        <div class="list_box_title">
+                                            <div class="top_title">
+                                                The Coca Cola
+                                            </div>
+                                            <div class="top_title_2">
+                                                Also known as covid -19 Antibody detection Test
+                                            </div>
+                                            <span class="rate_stare" style="color: #FAC213;"><i class="fa-solid fa-truck"></i>
+                                                Free
+                                                Delivery</span>
+                                            <div class="rate_stare">
+                                                <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i
+                                                    class="fa-solid fa-star"></i>
+                                                <i class="fa-solid fa-star"></i><i class="fa-solid fa-star-half"></i>
+                                                ( <i class="fa-regular fa-user"></i> 6899)
+                                            </div>
+                                            <div class="add_button_of_list">
+                                                <button class="btn active">Add to Cart</button>
+                                            </div>
+                                            
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        @endfor
-
+                            @endfor
+                    </div>
                     </div>
                 </div>
             </div>
@@ -146,7 +150,7 @@
             <span class="quantity_quantity">12 <i class="fa-solid fa-cubes-stacked"></i></span>
         </div>
 
-        <div class="add_button" style="width:29%;">
+        <div class="add_button" style="width:29%;" id="add_and_link">
             <button class="btn active">Add to Order</button>
         </div>
     </div>
